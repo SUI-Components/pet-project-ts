d37e01992f4a97fd0dfef00025d87a44753d8763
Carlos Villuendas Zambrana <carlosvillu@gmail.com>
2023-10-10 12:12:06 +0200

    refactor(qa/mocks): remove debugger

--- qa/mocks/routes/api.petproject.mpi-internals.com/user/:id/index.js
+++ qa/mocks/routes/api.petproject.mpi-internals.com/user/:id/index.js
@@ -1,5 +1,4 @@
 export async function get({headers, body, params, query, cookies}) {
-  debugger
   return [200, {name: 'nombre'}]
 }
 export async function post({headers, body, params, query, cookies}) {
