6d39eefc02e8d2fbc9bf1c9698b9ec61120e96a1
Carlos Villuendas Zambrana <carlosvillu@gmail.com>
2024-02-22 13:09:50 +0100

    fix lint

--- qa/e2e/integration/detail/index.js
+++ qa/e2e/integration/detail/index.js
@@ -2,7 +2,7 @@ import {URLS} from '../../fixtures/urls.js'
 
 describe('detail /Lift ', function () {
   beforeEach(function () {
-    cy.visit(URLS.HOME + '/movie/955916')
+    cy.visit(URLS.HOME + 'movie/955916')
   })
 
   it('Should render the name in the URL', function () {
