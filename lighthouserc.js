9d45d315da854cd58e5ad6d11f5b737b53b11bce
Carlos Villuendas Zambrana <carlosvillu@gmail.com>
2024-08-20 13:29:41 +0200

    chore(META): setup lhci

--- lighthouserc.js
+++ lighthouserc.js
@@ -6,6 +6,11 @@ module.exports = {
       url: [
         URL_APP + '/',              // eslint-disable-line
         URL_APP + '/movie/955916']  // eslint-disable-line
+    },
+    upload: {
+      target: 'lhci',
+      token: 'a8fcb41e-8af0-4374-8139-2a6849c43bb5',
+      serverBaseUrl: 'https://lhci-server.es-microtest-pro.schip.io/'
     }
   }
 }
